a32dcc18eeaf2f9e74d8b846924b3b0cc190c168
Stanislav Yurchenko <62614454+StanislavYurchenko@users.noreply.github.com>
2023-01-27 00:35:49 +0200

    1-feature: ferst setup (#1)

---
+++ apps/client-angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'easy-library-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client-angular';
+  dataFromServer = '';
+
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
+
+  getData() {
+    this.http
+      .get<{ message: string }>('/api')
+      .subscribe(({ message }) => ( this.dataFromServer = message ));
+  }
+}
