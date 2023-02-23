7e1cc57677763d005b27338e5a8e2ad719616cfc
Stanislav Yurchenko <yurchenko.stanislav@ukr.net>
2023-02-23 12:16:13 +0200

    21-featere: Autorization setup

--- apps/server-nest/src/app/modules/ability/guards/abilities.guard.ts
+++ apps/server-nest/src/app/modules/ability/guards/abilities.guard.ts
@@ -15,11 +15,8 @@ export class AbilitiesGuard implements CanActivate {
       const { user } = context.switchToHttp().getRequest();
       const ability = this.abilityFactory.defineAbility(user);
 
-      console.log('=> rules', rules);
-
       rules.forEach(rule => {
         ForbiddenError.from(ability).throwUnlessCan(rule.action, rule.subject, rule.field);
-        console.log('rule', rule);
       });
 
       return true;
