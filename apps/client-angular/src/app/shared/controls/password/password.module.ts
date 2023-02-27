df983de620ed0df2488d948eebc9d63d323a17da
Andrey Rybak <aliester.crowly@gmail.com>
2023-02-27 16:40:06 +0200

    add ivon hide

--- apps/client-angular/src/app/shared/controls/password/password.module.ts
+++ apps/client-angular/src/app/shared/controls/password/password.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordComponent } from './password.component';
+import {MatIconModule} from "@angular/material/icon";
 
 
 
 @NgModule({
     declarations: [PasswordComponent],
     imports: [
-        CommonModule
+        CommonModule,
+        MatIconModule
     ],
     exports: [
         PasswordComponent
